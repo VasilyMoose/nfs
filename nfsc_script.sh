be538549d7b6ea2ff2164a2e6a2d2a2c4100ca53
VasilyMoose <96911395+VasilyMoose@users.noreply.github.com>
2023-02-01 18:43:04 +0300

    Update nfsc_script.sh

--- nfsc_script.sh
+++ nfsc_script.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "192.168.50.10      nfss"  >> /etc/hosts
-mount nfss:/srv/share /mnt
-echo "nfss:/srv/share         /mnt    nfs     defaults,nofail 0 0" >> /etc/fstab
+mount nfss:/srv/share /mnt -o udp,nolock
+echo  "nfss:/srv/share  /mnt  nfs   nfsvers=3,proto=udp           0 0" >> /etc/fstab
+systemctl daemon-reload 
+
